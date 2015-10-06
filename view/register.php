a795a2de000e94d8b7a2a67cf7ee74023d133cdf
Prénom NOM <adresse@email.com>
2015-10-06 17:08:27 +0200

    Ajout des balises 'name' au formulaire d'inscription. #2

--- view/register.php
+++ view/register.php
@@ -12,22 +12,22 @@
 				<form method="POST" action="register.php">
 					<div class="form-input">
 						<span class="username">
-							<input type="text" placeholder="Username">
+							<input type="text" name="name" placeholder="Username">
 						</span>
 					</div>
 					<div class="form-input">
 						<span class="email">
-							<input type="text" placeholder="Email">
+							<input type="text" name="mail" placeholder="Email">
 						</span>
 					</div>
 					<div class="form-input">
 						<span class="password">
-							<input type="password" placeholder="Password">
+							<input type="password" name="password" placeholder="Password">
 						</span>
 					</div>
 
 					<div class="form-submit">
-						<input type="submit" value="Je m'inscris !">	
+						<input type="submit" name="inscription" value="Je m'inscris !">	
 					</div>
 					<p class="account">Vous avez un compte ? <a href="login.php">Connectez-vous !</a></p>
 				</form>
